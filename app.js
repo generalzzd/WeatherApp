cdd449ce7c7f4901157da8c493ea07a1b104f111
zidi <zidi@outlook.com>
2022-01-08 12:41:18 +0800

    add mapbox

--- app.js
+++ app.js
@@ -15,3 +15,12 @@ request({ url: url_geo, json: true }, (error, response) => {
     response.body.current.temperature
     + ' degree out. It feels like '+response.body.current.feelslike+' degree out.')
 })
+
+mapbox_url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZ2VuZXJhbHp6ZCIsImEiOiJja3k1YWp0dm8wazduMzFwZmR1NWdpcmh3In0.Rd0ZvMDKGwWkT6ywTJYcLg&limit=1';
+
+request({url: mapbox_url, json:true}, (error, response) =>{
+    console.log(response.body.features[0].text)
+    x = response.body.features[0].geometry.coordinates[0]
+    y = response.body.features[0].geometry.coordinates[1]
+    console.log(x+"|"+y)
+})
