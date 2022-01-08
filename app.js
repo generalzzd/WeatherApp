e8ba7022dafdafc4171cf7aa8a19a85ff9c01a95
zidi <zidi@outlook.com>
2022-01-08 19:26:22 +0800

    updaqte

--- app.js
+++ app.js
@@ -4,15 +4,22 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 geocode('boston', (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
+    
+    if(error){
+        console.log('Error', error)
+    }else {
+        console.log('Data', data)
+        forecast(data.x,data.y, (error, data) => {
+            if(console.error())
+                return console.log('Error', error)
+            console.log('Data', data)
+        })
+    }
+
 
 })
 
-forecast(40.7831,-73.9712, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
+
 
 
 console.log('System shutdown')
